3919f952313acd38e67d0ea922f8d94e72b2bc5c
Sami <seel63284@edu.ucl.dk>
2026-04-16 16:24:54 +0200

    Opdateret configjs

--- treesy-frontend/src/config.js
+++ treesy-frontend/src/config.js
@@ -1,2 +1,2 @@
-// src/config.js
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5106';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5106';
+export default API_BASE_URL;
